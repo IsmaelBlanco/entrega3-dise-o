d783709ddcd783bfb31585d827227d7adef4091d
Ismael <iblancodelac2401@educantabria.es>
2025-12-21 18:32:22 +0100

    pagina de registro/login terminada; faltaria responsividad pero no da tiempo a todo

---
+++ assets/script/login.js
@@ -0,0 +1,18 @@
+const box = document.getElementById('mainBox');
+const title = document.getElementById('title');
+const desc = document.getElementById('desc');
+const btn = document.getElementById('swpbtn');
+
+function toggle() {
+    box.classList.toggle('active');
+    const isRegister = box.classList.contains('active');
+
+    // Cambiamos el texto de la cortina según donde esté
+    title.innerText = isRegister ? "¿Ya tienes cuenta?" : "¿Eres nuevo?";
+    desc.innerText = isRegister ? "Inicia sesión aquí" : "Regístrate para ver más contenido";
+    btn.innerText = isRegister ? "Aún no tengo cuenta" : "Ya tengo cuenta";
+}
+
+function enviar(tipo) {
+    //Aquí iría la lógica AJAX futura (fetch)
+}
